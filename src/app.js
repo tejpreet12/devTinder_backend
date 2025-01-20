2157c795aff3bedf83fc8aa1ca8698f88e663dc7
tejpreet12 <tejpreetdhiman429@gmail.com>
2025-01-20 22:51:51 +0530

    Worked_with_get_patch_post_delete

--- src/app.js
+++ src/app.js
@@ -2,22 +2,34 @@ const express = require("express");
 
 const app = express();
 
+// This will only handle get call for /user
+app.get("/user", (req,res) => {
+    res.send({
+        firstName:'Akshay',
+        lastName:"Saini",
+    })
+})
+
+app.post("/user", (req,res) => {
+    console.log("Data Saved to DB");
+    res.send("Data Saved Successfully");
+})
+
+app.delete("/user", (req,res)=> {
+    console.log("Deleting the user");
+    res.send("User Deleted");
+})
+
+app.patch("/user", (req,res) =>{
+    console.log("Data Updated Successfully");
+    res.send("Data Updated")
+})
+
+
+// this will match all the Http method api calls to /test
 app.use("/test", (req, res) => {
     res.send("Test Page Important");
 })
-
-app.use ("/hello",(req,res) => {
-    res.send("Hello From Server.");
-})
-
-app.use ("/namaste",(req,res) => {
-    res.send("Namaste From Server.");
-})
-
-app.use ("/",(req,res) => {
-    res.send("Dashboard Page");
-})
-
 
 app.listen(7777, () => {
     console.log("Server is running successfully on Port 7777");
