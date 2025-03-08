d9d1ff2aacc9e902f5d7a5dcc01e7737638b7cae
tejpreet12 <tejpreetdhiman429@gmail.com>
2025-03-08 14:31:46 +0530

    FeedApi-Pagination

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const  {validateSignUpData} = require("../utils/validator")
 
 const authRouter = express.Router();
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { authUser } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const Users = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -37,9 +38,9 @@ userRouter.get("/user/connections", authUser, async (req, res) => {
       .populate("toUserId", USER_SAFE_DATA);
 
     const data = ConnectionRequests.map((data) => {
-      if(loggedInUser._id.toString() === data.fromUserId._id.toString()){
+      if (loggedInUser._id.toString() === data.fromUserId._id.toString()) {
         return data.toUserId;
-      } 
+      }
       return data.fromUserId;
     });
 
@@ -47,6 +48,39 @@ userRouter.get("/user/connections", authUser, async (req, res) => {
   } catch (err) {
     res.status(400).send("Err :" + err.message);
   }
+});
+
+userRouter.get("/feed", authUser, async (req, res) => {
+  const loggedInUser = req.user;
+
+  const pageNumber = parseInt(req.query.pageNo) || 1;
+  let limit = parseInt(req.query.limit) || 2;
+  limit = limit > 50 ? 2 : limit;
+  const skip = (pageNumber-1) * limit;
+
+
+  const ConnectionRequests = await ConnectionRequest.find({
+    $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+  })
+    .select("fromUserId toUserId")
+    .populate("fromUserId", USER_SAFE_DATA)
+    .populate("toUserId", USER_SAFE_DATA);
+
+  const hideUsersFromFeed = new Set();
+
+  ConnectionRequests.forEach((req) => {
+    hideUsersFromFeed.add(req.fromUserId._id.toString());
+    hideUsersFromFeed.add(req.toUserId._id.toString());
+  });
+  
+  const users = await Users.find({
+    $and: [
+      { _id: { $nin: Array.from(hideUsersFromFeed) } },
+      // { _id: { $ne: loggedInUser._id } },
+    ],
+  }).skip(skip).limit(limit);
+
+  res.json({ UsersData: users });
 });
 
 module.exports = { userRouter };
